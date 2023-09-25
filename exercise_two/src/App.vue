0eedcc15635fd9259ee30342cbc746afa9a652f1
VictorMRD <victormrd2002@hotmail.com>
2023-09-25 13:45:20 -0700

    CRUD

--- exercise_two/src/App.vue
+++ exercise_two/src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
   import AddUsers from './components/AddUsers.vue';
   import TableUsers from './components/tableUsers.vue';
-  import Login from './components/login.vue';
+  import Login from './components/Login.vue';
   import { ref } from 'vue';
 
   const isLoggedIn = ref(false);
 
   const handleLoginSuccess = (isSuccessful) => {
     isLoggedIn.value = isSuccessful;
+  };
+  const receiveUserData = (data) => {
+    userData.value.push(data);
   };
 </script>
 
@@ -19,8 +22,8 @@
   <main>
     <div class="mainContainer">  
       <Login @login-success="handleLoginSuccess" />
-      <AddUsers v-if="isLoggedIn" />
-      <TableUsers v-if="isLoggedIn" />
+      <AddUsers v-if="isLoggedIn" @user-added="receiveUserData"/>
+      <TableUsers v-if="isLoggedIn" :usersData="userData"/>
     </div>
   </main>
 </template>
