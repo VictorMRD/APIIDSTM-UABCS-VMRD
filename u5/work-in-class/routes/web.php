f6d749927203d8c14fa0a76849f21f437ba15469
VictorMRD <victormrd2002@hotmail.com>
2023-11-02 13:47:46 -0700

    solved problem with local hosting and validated routes

--- u5/work-in-class/routes/web.php
+++ u5/work-in-class/routes/web.php
@@ -17,6 +17,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/* Ejercicio de operaciones basicas */
+Route::get('/resta/{nf?}/{ns?}', function (int $nf=0, $ns=0) {
+    return 'Resultado: ' .$nf - $ns;
+})->where(['nf' => '[0-9]+', 'ns' => '[0-9]+']);
+Route::get('/suma/{nf?}/{ns?}', function (int $nf=0, $ns=0) {
+    return 'Resultado: ' .$nf + $ns;
+})->where(['nf' => '[0-9]+', 'ns' => '[0-9]+']);
+Route::get('/multiplicacion/{nf?}/{ns?}', function (int $nf=0, $ns=0) {
+    return 'Resultado: ' .$nf * $ns;
+})->where(['nf' => '[0-9]+', 'ns' => '[0-9]+']);
+Route::get('/division/{nf?}/{ns?}', function (int $nf=0, $ns=0) {
+    return 'Resultado: ' .$nf / $ns;
+})->where(['nf' => '[0-9]+', 'ns' => '[0-9]+']);
+
 Route::get('/main/{var?}', function (string $var="victor") {
     /* De esta manera decimos que los datos de un parametro son mandados a la vista
         Asi las variables que aqui recibimos pueden ser utilizadas en la vista*/
@@ -26,16 +40,13 @@ Route::get('/main/{var?}', function (string $var="victor") {
 /* el ? al final de una variable nos indiica que el parametro puede ser opcional.
     Esto sirve para que en caso de que el parametro no sea enviado la ruta no devuelva un error.
     Para poder hacer esto, tenemos que declarar una valor predefinido por deault en la varriable. */
-Route::get('/saludo/{nombre}/{lastname?}', function (string $nombre, $lastname ="Doe") {
+Route::get('/saludo/{nombre?}/{lastname?}', function (string $nombre ="", $lastname ="") {
     return 'Hola ' .$nombre. ' ' .$lastname;
     /* Where nos permite validar el tipo de dato que se puede ingresar por la ruta 
     Podemos ver los diferentes tipos de datos que podemos validar en la documentacion de las rutas en la pagina oficial
     de Laravel */
 })->where(['nombre' => '[a-z]+', 'lastname' => '[a-z]+']);
 
-Route::get('/suma/{nf?}/{ns?}', function (int $nf=0, $ns=0) {
-    return 'Resultado: ' .$nf + $ns;
-});
 
 use App\Http\Controllers\UserController;
 Route::get('/users', [UserController::class, 'index']);
